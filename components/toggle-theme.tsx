5b6b8540fff68185dfa6e62042ace2efd9d52e2c
Lalu Ahmad Ardiansyah <ardian.dev.stuff@gmail.com>
2023-08-03 00:11:56 +0800

    tidy

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -4,10 +4,15 @@ import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
+enum Theme {
+  Light = 'light',
+  Dark = 'dark',
+}
+
 export default function ToggleTheme() {
   const [mounted, setMounted] = useState(false)
   const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme === 'system' ? systemTheme : theme
+  const currentTheme = (theme === 'system' ? systemTheme : theme) as Theme
 
   useEffect(() => {
     setMounted(true)
@@ -15,17 +20,18 @@ export default function ToggleTheme() {
 
   if (!mounted) return null
 
+  const toggleThemeIcon = {
+    light: <SunIcon className="h-5 w-5" />,
+    dark: <MoonIcon className="h-5 w-5" />,
+  }[currentTheme]
+
   return (
     <button
-      onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
-      className="rounded-md p-2 duration-150 hover:bg-gray-100 dark:hover:bg-white dark:hover:bg-opacity-10"
+      onClick={() => setTheme(currentTheme === 'light' ? 'dark' : 'light')}
+      className="rounded-md p-2 transition-all hover:bg-gray-100 dark:hover:bg-white dark:hover:bg-opacity-10"
       aria-label="Switch theme"
     >
-      {currentTheme === 'light' ? (
-        <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      ) : (
-        <MoonIcon className="h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      )}
+      {toggleThemeIcon}
     </button>
   )
 }
